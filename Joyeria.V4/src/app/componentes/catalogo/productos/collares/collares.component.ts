a178b2643a0d035e0be1505526f0ad06856b7bf7
Gianfranco26 <gbozzod@udd.com>
2020-10-19 19:59:42 -0300

    Catalogo y Carrito integrados_10_20

---
+++ Joyeria.V4/src/app/componentes/catalogo/productos/collares/collares.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-collares',
+  templateUrl: './collares.component.html',
+  styleUrls: ['./collares.component.css']
+})
+export class CollaresComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ Joyeria.V4/src/app/models/accesory.ts
@@ -0,0 +1,7 @@
+export interface Accesory {
+    $key: string;
+    title: string;
+    price: number;
+    category: string;
+    imageUrl: string;
+}
