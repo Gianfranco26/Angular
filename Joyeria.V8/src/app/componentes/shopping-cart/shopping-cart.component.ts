4a548b8dfb0fe43877fef383ac1af4a8d84af577
Gianfranco26 <gbozzod@udd.com>
2020-11-14 16:57:46 -0300

    Check out, Mis ordenes y Ordenes detail integrados

---
+++ Joyeria.V8/src/app/componentes/shopping-cart/shopping-cart.component.ts
@@ -0,0 +1,26 @@
+import { Observable } from 'rxjs/Observable';
+import { Component, OnInit } from '@angular/core';
+import { ShoppingCartService } from './../../shopping-cart.service';
+import { ShoppingCart } from '../../models/shopping-cart';
+
+
+@Component({
+  selector: 'app-shopping-cart',
+  templateUrl: './shopping-cart.component.html',
+  styleUrls: ['./shopping-cart.component.css']
+})
+export class ShoppingCartComponent implements OnInit {
+  cart$: Observable<ShoppingCart>;
+  constructor(private shoppingCartService: ShoppingCartService) { }
+
+  async ngOnInit() {
+    this.cart$ = await this.shoppingCartService.getCart();
+    
+
+  }
+
+  clearCart() {
+    this.shoppingCartService.clearCart();
+  }
+
+}
